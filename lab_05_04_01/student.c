ce9d17cb85a4d76cdae189669b33bacb79156cb4
Orlov Aleksei <orlovav@student.bmstu.ru>
2023-05-29 22:39:10 +0300

    task 4 print everything on a new line

--- lab_05_04_01/student.c
+++ lab_05_04_01/student.c
@@ -2,7 +2,7 @@
 
 int print_student(student_struct *student)
 {
-    printf("%s\n%s\n%d %d %d %d\n", student->surname, student->name,
+    printf("%s\n%s\n%d\n%d\n%d\n%d\n", student->surname, student->name,
     student->marks[0], student->marks[1], student->marks[2], student->marks[3]);
     return EXIT_SUCCESS;
 }
